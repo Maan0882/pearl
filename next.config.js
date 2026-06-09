17ddc06c188ee5b4238254cc8b6fd629914ec79e
Maan0882 <gajjarmansi2808@gmail.com>
2026-06-09 14:41:36 +0530

    fix: update next.config.js to include unoptimized images configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  allowedDevOrigins: ["192.168.1.14"],
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
